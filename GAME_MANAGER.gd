e3c925d1df79f63bc3e843903ebccbb8009f5f55
Just Another Indie <justanotherindiegamer@gmail.com>
2026-07-26 22:06:38 -0600

    Added Game Manager and Scoring

---
+++ GAME_MANAGER.gd
@@ -0,0 +1,5 @@
+extends Node
+
+
+var right_player_score: int
+var left_player_score: int
--- level.gd
+++ level.gd
@@ -10,3 +10,12 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		get_tree().reload_current_scene()
+
+
+func _on_left_paddle_scoring_area_body_exited(body: Node2D) -> void:
+	GameManager.left_player_score += 1
+	print("Left Scored %d" % GameManager.left_player_score  )
+	
+func _on_right_paddle_scoring_area_body_exited(body: Node2D) -> void:
+	GameManager.right_player_score += 1
+	print("Right Scored %d" % GameManager.right_player_score)
